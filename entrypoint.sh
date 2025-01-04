889af55a837cda923a0e334b6395b1d3d4e054b3
TRAN HAI NAM <toilatranhainam@gmail.com>
2025-01-04 21:37:50 +0700

    Update entrypoint.sh
    any commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# Sleep for 2 seconds to ensure container is ready
+
+sleep 2
+tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 sleep 2
 
-cd /home/container
-MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+tailscale up --auth-key=tskey-auth-kZey9nf7af11CNTRL-QKxPzzCTLwA7baku5NvnwAYaFXtmqvUw || exit 1
 
-# Make internal Docker IP address available to processes.
 export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
-# Check if already installed
+cd /home/container
+
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+
 if [ ! -e "$HOME/.installed" ]; then
     /usr/local/bin/proot \
     --rootfs="/" \
@@ -18,5 +21,4 @@ if [ ! -e "$HOME/.installed" ]; then
     /bin/bash "/install.sh" || exit 1
 fi
 
-# Run the startup helper script
 bash /helper.sh
